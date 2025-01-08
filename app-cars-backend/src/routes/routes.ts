a94acde2c55e7761c93988e4d1202e86a86d6189
Wilson Goal BUGIRI <bugiriwilson651@gmail.com>
2025-01-08 20:01:51 +0200

    modified routes

--- app-cars-backend/src/routes/routes.ts
+++ app-cars-backend/src/routes/routes.ts
@@ -3,12 +3,10 @@ import userController from '../controllers/RegistrationController'
 
 const router = Router()
 
-router.get('/', userController.testing)
 //basic creating
 router.post('/signUp', userController.signUp)
 router.post('/registerMechanic', userController.registerMechanic)
-router.post('/registerGarageOwner', userController.registerGarageOwner)
 router.post('/registerGarage', userController.registerGarage)
-router.post('/signIn', userController.signIn)
+
 
 export default router
