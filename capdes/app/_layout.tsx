25e09e98ec5f6f71e28592270bafa18e145f8981
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2025-01-24 07:36:55 +0200

    modifying the frontend to the new version

--- capdes/app/_layout.tsx
+++ capdes/app/_layout.tsx
@@ -1,48 +1,21 @@
-import { useFonts } from "expo-font";
-import { Stack, useRouter } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import "react-native-reanimated";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
 
-import { ThemeProvider } from "@/context/ThemeContext";
-import { SettingsProvider } from "@/context/SettingsContext";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-  const [loggedInUser, setLoggedInUser] = useState<null | string>("Mukami");
-  const router = useRouter()
-
-  const checkIfUserIsLoggedIn = async () => {
-    try {
-      const userData = await AsyncStorage.getItem("savedUser");
-      console.log("User Data: ", userData);
-      if (userData && userData != undefined) {
-        setLoggedInUser(userData);
-      } else {
-        setLoggedInUser(null);
-      }
-    } catch (e) {
-      console.error("Error getting logged In user ", e);
-    }
-  };
-
-  // useEffect(() => {
-  //   checkIfUserIsLoggedIn();
-
-  //   if (loggedInUser) {
-  //     router.replace("/(authenticated)");
-  //   } else {
-  //     router.replace("/(auth)/login");
-  //   }
-  // }, [loggedInUser, router]);
 
   useEffect(() => {
     if (loaded) {
@@ -55,20 +28,12 @@ export default function RootLayout() {
   }
 
   return (
-    <GestureHandlerRootView>
-      <ThemeProvider>
-        <SettingsProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(authenticated)" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-            {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
-          </Stack>
-          <StatusBar style="auto" />
-        </SettingsProvider>
-      </ThemeProvider>
-    </GestureHandlerRootView>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
--- capdes/constants/Colors.ts
+++ capdes/constants/Colors.ts
@@ -1,45 +1,26 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
+
 export const Colors = {
-     light: {
-          mainColor: "#FFB300",
-          headerStartBg: { first: "#111", second: "#111", third: "#111" },
-          mainHeaderColor: "#111",
-          textColor: "#111",
-          secondTextColor: "#111",
-          linkColor: "#FFB300",
-          mainIconColor: "#111",
-          secondIconColor: "#FFF",
-          mainRadius: 5,
-          sideRadius: 30,
-          background: { first: "#CCCCFF", second: "#DDDDFFcc", third: "#EEEEFFaa" },
-          footerBg: "#222",
-     },
-     dark: {
-          mainColor: "#55FFFF",
-          headerStartBg: { first: "#212F42", second: "#333F60", third: "#454F66" },
-          mainHeaderColor: "#333F55",
-          textColor: "#FFF",
-          secondTextColor: "#FFF",
-          linkColor: "#55FFFF",
-          mainIconColor: "#EEE",
-          secondIconColor: "#333F55",
-          mainRadius: 20,
-          sideRadius: 30,
-          background: { first: "#212F42", second: "#333F60", third: "#454F66" },
-          footerBg: "#333F55",
-     },
-     custom: {
-          mainColor: "#440066",
-          headerStartBg: { first: "#330055", second: "#440088", third: "#660099" },
-          mainHeaderColor: "#441166",
-          textColor: "#FFF",
-          secondTextColor: "#111",
-          linkColor: "#440066",
-          mainIconColor: "#666",
-          secondIconColor: "#999",
-          mainRadius: 10,
-          sideRadius: 30,
-          background: { first: "#EEE", second: "#FFF", third: "#DDD" },
-          // background: { first: "#330055", second: "#440088", third: "#660099" },
-          footerBg: "#441166",
-     }
+  light: {
+    text: '#11181C',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+  },
 };
